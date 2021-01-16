1e5161b2be5b51992ed0f7deaf72a66d9f83667c
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-16 14:26:49 +0100

    Extract Avatar, Header, Content and Footer components from Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,46 @@
-function CardList() {
+function Avatar() {
     return (
-        <div className="card">
-            <div className="avatar">
-                <img alt="avatar" src="https://via.placeholder.com/50" />
-                <p>
-                    <span>name@123</span>
-                    <span className="date">a day ago</span>
-                </p>
+        <div className="avatar">
+            <img alt="avatar" src="https://via.placeholder.com/50" />
+            <p>
+                <span>name@123</span>
+                <span className="date">a day ago</span>
+            </p>
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div className="block">
+            <Avatar />
+
+            <div className="title">title</div>
+
+            <div className="block tags">
+                <span>#tag1</span>
+                <span>#tag2</span>
+                <span>#tag2</span>
             </div>
+        </div>
+    );
+}
 
-            <div className="block">
-                <div className="title">title</div>
+function Content() {
+    return (
+        <div>
+            <figure>
+                <img alt="Resouce" src="https://via.placeholder.com/600x400" />
+                <figcaption>A web resource image or video</figcaption>
+            </figure>
+        </div>
+    );
+}
 
-                <div className="block tags">
-                    <span>#tag1</span>
-                    <span>#tag2</span>
-                    <span>#tag2</span>
-                </div>
-            </div>
-
-            <div>
-                <figure>
-                    <img alt="150" width="600" height="400" src="https://via.placeholder.com/600x400" />
-                    <figcaption>A web resource image or video</figcaption>
-                </figure>
-            </div>
-
-            <div className="social">
+function Footer() {
+    return (
+        <div className="social">
+            <div className="control">
                 <label>
                     <button>Like</button> 12
                 </label>
@@ -40,12 +54,21 @@ function CardList() {
                     <button>Share</button> 12
                 </label>
             </div>
-
             <div>
-                View: 11.1k
+                View: 11.1K
             </div>
         </div>
     );
 }
 
-export default CardList;
+function Card() {
+    return (
+        <div className="card">
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    );
+}
+
+export default Card;

b5a9c198d22dc091d61248a0313b2e9a9ea6a2db
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-18 15:27:35 +0100

    Add util.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './css/util.css';
 import './css/App.css';
 import './css/minireset.min.css';
 import banner from './images/potw-banner.png';
@@ -47,7 +48,7 @@ class App extends Component {
         return (
             <Fragment>
                 <Navbar />
-                <main className="main pt-3 mt-1">
+                <main className="main is-flex pt-3 mt-1">
                     <Suggestions />
                     <section className="section">
                         <Search />
@@ -63,8 +64,8 @@ class App extends Component {
 
 function Suggestions() {
     return (
-        <div className="container left flex-grow">
-            <p className="has-background-white pt-4 px-3 box">Suggestions</p>
+        <div className="aside container left">
+            <p className="aside-content has-background-white pt-4 px-3 box">Suggestions</p>
         </div>
     );
 }
@@ -73,16 +74,36 @@ function Hashtags(props) {
     const { toptags } = props;
 
     return (
-        <div className="container right flex-column no-gap flex-start flex-grow">
-            Top Hashtags
-            <div className="has-background-white box banner-container is-flex flex-column">
-                <p className="py-4 px-3 is-flex flex-wrap">
-                    {toptags.map((tag, id) => <a href="#" key={id}>{tag}</a>)}
-                </p>
-                <figure className="flex-grow banner">
-                    <img width="300" alt="Tipestry post of the week event"
-                        src={banner} />
-                </figure>
+        <div className="aside container right">
+            <div className="aside-content">
+                <div className="aside-fixed is-flex flex-column">
+                    <div>
+                        <p>Top Hashtags</p>
+                        <div className="has-background-white box banner-container is-flex flex-column">
+                            <p className="py-4 px-3 is-flex flex-wrap">
+                                {toptags.map((tag, id) => <a className="tag" href="/" key={id}>{tag}</a>)}
+                            </p>
+                            <img width="300" alt="Tipestry post of the week event"
+                                src={banner} />
+                        </div>
+                    </div>
+
+                    <div className="policy box has-background-white py-3 px-3 is-flex flex-column">
+                        <p>
+                            <strong className="emphasis">© 2021 <em>Tipestry</em></strong> Faq Contact About Contests Privacy Policy
+                        Tipestry Go White Paper Tipestry for Chrome Terms and Condition
+                        </p>
+                        <div>
+                            <p>Follow Us on Social Media</p>
+                            <div className="social pt-5 is-flex">
+                                <p><a className="link" href="/">Facebook</a></p>
+                                <p><a className="link" href="/">Twiter</a></p>
+                                <p><a className="link" href="/">Youtube</a></p>
+                                <p><a className="link" href="/">Telegram</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );

bbc985ff4059bb939f3f7eb75b7c3f983d775838
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-19 12:00:11 +0100

    Add RegisterDialog component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import RegisterDialog from './RegisterDialog';
+
+import React, { Fragment } from 'react';
 
 const Navbar = () => {
     return (
-        <div className="navbar has-background-link py-4 px-5">
-            <div className="container">
-                <div className="brand">
-                    <span className="subtitle">
-                        Tipestry
+        <Fragment>
+            <div className="navbar has-background-link py-4 px-5">
+                <div className="container">
+                    <div className="brand">
+                        <span className="subtitle">
+                            Tipestry
                     </span>
 
+                    </div>
+                    <nav className="nav-items is-flex">
+                        <a className="nav-item btn py-5 px-5" href="/">Post</a>
+                        <a className="nav-item btn py-5 px-5" href="/">Login</a>
+                        <a className="nav-item btn py-5 px-5" href="#register-dialog">Join Us</a>
+                        <button className="nav-item btn py-5 px-5">en</button>
+                    </nav>
                 </div>
-                <nav className="navItem">
-                    <button>Post</button>
-                    <button>Login</button>
-                    <button>Join Us</button>
-                    <button>en</button>
-                </nav>
             </div>
-        </div>
+            <RegisterDialog id="register-dialog" />
+        </Fragment>
     );
 }
 
---
+++ src/components/RegisterDialog.jsx
@@ -0,0 +1,53 @@
+import '../css/modal.css';
+
+const Input = ({ label, type, name, placeholder }) => {
+    return (
+        <div className="control is-flex">
+            <label for={name} className="label">{label}:</label>
+            <input className="input" id={name} type={type} name={name} placeholder={placeholder} />
+        </div>
+    );
+};
+
+const RegisterDialog = ({ id }) => {
+    return (
+        <div id={id} class="modal-window">
+            <form>
+                <div className="form is-flex flex-column box has-background-white pt-2 pb-4 px-2">
+
+                    <p className="subtitle has-color-link flex-grow mb-1">Register User</p>
+
+                    <Input
+                        label="Username"
+                        type="text"
+                        name="name"
+                        placeholder="Enter username" />
+
+                    <Input
+                        label="Email"
+                        type="email"
+                        name="email"
+                        placeholder="Enter email address" />
+
+                    <Input
+                        label="Password"
+                        type="password"
+                        name="password"
+                        placeholder="Enter password" />
+
+                    <Input
+                        label="Confirm Password"
+                        type="password"
+                        placeholder="Confirm password" />
+
+                    <div className="control buttons is-flex mt-3">
+                        <a href="/" title="Close" className="close modal-close has-color-black">Cancel</a>
+                        <input className="btn py-4 px-3 is-primary" type="submit" value="Create" />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default RegisterDialog;

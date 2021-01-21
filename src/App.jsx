a5e19fa85b2cbf13451bbed3a1226bedae994394
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-21 14:27:39 +0100

    Extract Suggestions and Hashtags from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './css/util.css';
 import './css/App.css';
 import './css/minireset.min.css';
-import banner from './images/potw-banner.png';
 
 import React, { Component, Fragment } from 'react';
 
+import Hashtags from './components/Hashtags';
+import Suggestions from './components/Suggestions';
 import Register from './components/Register';
 import LoginDialog from './components/LoginDialog';
 import PostDialog from './components/PostDialog';
@@ -94,53 +95,6 @@ class App extends Component {
             </Fragment>
         );
     }
-}
-
-const Suggestions = () => {
-    return (
-        <div className="aside container left">
-            <p className="aside-content has-background-white pt-4 px-3 box">Suggestions</p>
-        </div>
-    );
-}
-
-const Hashtags = (props) => {
-    const { toptags } = props;
-
-    return (
-        <div className="aside container right">
-            <div className="aside-content">
-                <div className="aside-fixed is-flex flex-column">
-                    <div>
-                        <p>Top Hashtags</p>
-                        <div className="has-background-white box banner-container is-flex flex-column">
-                            <p className="py-4 px-3 is-flex flex-wrap">
-                                {toptags.map((tag, id) => <a className="tag" href="/" key={id}>{tag}</a>)}
-                            </p>
-                            <img width="300" alt="Tipestry post of the week event"
-                                src={banner} />
-                        </div>
-                    </div>
-
-                    <div className="policy box has-background-white py-3 px-3 is-flex flex-column">
-                        <p>
-                            <strong className="emphasis">© 2021 <em>Tipestry</em></strong> Faq Contact About Contests Privacy Policy
-                        Tipestry Go White Paper Tipestry for Chrome Terms and Condition
-                        </p>
-                        <div>
-                            <p>Follow Us on Social Media</p>
-                            <div className="social pt-5 is-flex">
-                                <p><a className="link" href="/">Facebook</a></p>
-                                <p><a className="link" href="/">Twiter</a></p>
-                                <p><a className="link" href="/">Youtube</a></p>
-                                <p><a className="link" href="/">Telegram</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
 }
 
 export default App;
---
+++ src/components/Suggestions.jsx
@@ -0,0 +1,9 @@
+const Suggestions = () => {
+    return (
+        <div className="aside container left">
+            <p className="aside-content has-background-white pt-4 px-3 box">Suggestions</p>
+        </div>
+    );
+}
+
+export default Suggestions;

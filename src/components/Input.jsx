f5e6dde94b1c8fd810ba5dc5a2abe9770fc76d22
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-21 13:21:18 +0100

    Add id param to 'createInput'

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
 
 const Input = (props) => {
-    const {
-        label,
-        type,
-        name,
-        value,
-        placeholder,
-        onInput,
-        hasError } = props;
+    const { id, label, type, name, value, placeholder, onInput, hasError } = props;
 
-    const errorClasses = "error" + (hasError ? "" : " is-not-visible");
+    const errorClasses = "error size-medium";
 
     return (
-        <div className="control is-flex flex-column">
-            <span className={errorClasses}>{hasError}</span>
-
-            <fieldset>
-                <legend>{label}</legend>
-                <input className="input"
-                    id={name}
+        <div className="input-container control">
+            <fieldset className="is-flex flex-column field py-5 px-5">
+                <span className={errorClasses}>{hasError}</span>
+                <legend className="legend size-medium has-color-grey">{label}</legend>
+                <input className="input py-4 px-4 size-medium"
+                    id={id ? id : name}
                     type={type}
                     name={name}
                     value={value}
@@ -31,12 +23,12 @@ const Input = (props) => {
     );
 };
 
-export const createInput = (name, type, label, placeholder) => {
+export const createInput = (name, type, label, placeholder, id = "") => {
     return (props) => {
         const { value, hasError, onChange } = props;
 
         return (
-            <Input
+            <Input id={id ? id : name}
                 label={label}
                 type={type}
                 name={name}

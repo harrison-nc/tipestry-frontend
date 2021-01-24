d2d6a00cf073f12caafb429f66292ae1ae36d51a
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-24 11:19:08 +0100

    Refactor Login component
    Refactor Login using InputHooks and Form input components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,212 +1,148 @@
 import '../css/modal.css';
-
 import React, { useState } from 'react';
+import { Email, Password } from './Input';
+import { validateEmail } from '../util/validators';
+import { FormModal, createModal } from './modal/Modal';
+import { useFormInputWithValidator } from './hooks/InputHooks';
 
-import { createValidator, validateEmail } from '../util/validators';
-
-import Input from './Input';
-import Modal from './Modal';
-import { FormModal } from './Modal';
+const useFormInput = useFormInputWithValidator;
 
 const Login = (props) => {
     const { id, onLogin } = props;
-
-    const [email, setEmail] = useState('');
-    const [emailError, setEmailError] = useState('');
-
-    const [password, setPassword] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-
+    const Inputs = useInputs();
     const [serverError, setServerError] = useState('');
-
-    const Validators = {
-        email: {
-            ...createValidator(value => {
-                if (!value) return 'Please enter an email.';
-                else if (!validateEmail(value)) return 'Please enter a valid email.';
-                else return '';
-            }),
-            setValue: setEmail,
-            setError: setEmailError,
-        },
-
-        password: {
-            ...createValidator(value => {
-                if (!value) return 'Please enter a password.';
-                else return '';
-            }),
-            setValue: setPassword,
-            setError: setPasswordError,
-        },
-
-        resetValues() {
-            setEmail('');
-            setPassword('');
-        },
-
-        resetErrors() {
-            setEmailError('');
-            setPasswordError('');
-        }
-    };
-
-    const isValid = (name, value) => {
-        const input = Validators[name];
-
-        if (input) return input.isValid(value);
-
-        else return false;
-    };
-
-    const validateValues = () => {
-        return isValid('email', email)
-            && isValid('password', password);
-    };
-
-    const checkValidity = (name, value) => {
-        const input = Validators[name];
-
-        if (!input) return console.log(`'${input}' is not valid`);
-
-        input.validate(value);
-    };
-
-    const resetInput = () => Validators.resetValues();
-
-    const resetError = (name) => {
-        const input = Validators[name];
-
-        if (input) input.setError('');
-
-        else if (serverError) setServerError('');
-
-        else Validators.resetErrors();
-    }
 
     const showError = (error) => {
         const { key, value, message } = error;
-        checkValidity(key, value, message);
+        const input = Inputs[key];
+        if (input) input.setError(message);
+        else console.error(`Server Error: ${message}`);
+        console.log(`Error: '${key}' - '${value}`);
     };
 
-    const reportValidity = () => {
-        resetError();
-
-        checkValidity('email', email);
-        checkValidity('password', password);
+    const handleClear = (e) => {
+        Inputs.asArray().forEach(input => input.reset());
+        setServerError('');
     };
 
-    const handleBlur = ({ target }) => checkValidity(target.name, target.value);
-
-    const handleFocus = ({ target }) => resetError(target.name);
-
-    const handleReset = (e) => {
-        resetInput();
-        resetError();
+    const handleClose = (e) => {
+        handleClear(e);
+        window.location.hash = '#app';
     }
+
+    const handleSubmitFailure = (error) => {
+        const { login } = error;
+
+        if (login) setServerError(login.message);
+
+        else if (error instanceof Error) setServerError('Unable to login at this moment.');
+
+        else if (error instanceof Array) error.forEach(err => showError(err));
+
+        else if (error instanceof Object) showError(error);
+
+        else window.location.href = '#lfailure';
+
+        console.error(error);
+    };
+
+    const handleSubmitSuccess = () => {
+        handleClear();
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const isValid = validateValues();
+        const isValid = Inputs.validateAll();
 
-        if (isValid) try {
-            const { error } = await onLogin({ email, password });
+        if (!isValid) return;
 
-            if (error) {
+        try {
+            const { error } = await onLogin({
+                email: Inputs.email.getValue(),
+                password: Inputs.password.getValue(),
+            });
 
-                const { login } = error;
-
-                if (login) setServerError(login.message);
-
-                else if (error instanceof Array) error.forEach(err => showError(err));
-
-                else if (error instanceof Object) showError(error);
-
-                else window.location.href = '#lfailure';
-
-                console.error(error);
-            }
-
-            else resetError() && resetInput();
+            if (error) handleSubmitFailure(error);
+            else handleSubmitSuccess();
 
         } catch (ex) {
-            window.location.href = '#lfailure';
+            handleSubmitFailure(ex);
             console.error(ex);
         }
-
-        else reportValidity();
-
-        return null;
     }
 
-    const handleChange = ({ target }) => {
-        const { name, value } = target;
+    const closeButton = <input className="close modal-close btn" type="reset" value="X" />
 
-        const input = Validators[name];
-
-        if (input) input.setValue(value);
-
-        else console.error(`'${name}' invalid input`);
-    }
+    const form = {
+        id,
+        title: "Sign in",
+        method: "post",
+        close: closeButton,
+        onReset: handleClose,
+        onSubmit: handleSubmit,
+    };
 
     return (
         <div className="login">
-            <FormModal id={id}
-                title="Sign in"
-                method="post"
-                close={<Close />}
-                onReset={handleReset}
-                onSubmit={handleSubmit}>
-
-                <Input
-                    label="Email"
-                    type="email"
-                    name="email"
-                    value={email}
-                    hasError={emailError}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    onFocus={handleFocus}
-                    placeholder="Enter email address" />
-
-                <Input
-                    label="Password"
-                    type="password"
-                    name="password"
-                    value={password}
-                    hasError={passwordError}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    onFocus={handleFocus}
-                    placeholder="Enter password" />
+            <FormModal {...form}>
+                <Email {...Inputs.email.props} />
+                <Password {...Inputs.password.props} />
 
                 {serverError && <span className="error">{serverError}</span>}
 
-                <div className="control buttons is-flex mt-3">
-                    <input className="btn py-4 px-3 is-primary" type="submit" value="Login" />
-                </div>
+                <Control onClear={handleClear} />
             </FormModal>
-
-            <Modal id="lfailure" classes="box hax-background-white">
-                <div className="failure is-flex flex-column box has-background-white py-3 px-3">
-                    <p className="flex-grow is-flex subtitle">
-                        Login failed
-                    </p>
-                    <p className="close-container flex-grow is-flex">
-                        <a className="modal-close close is-info mb-1" alt="close modal" href="#app">
-                            Close
-                        </a>
-                    </p>
-                </div>
-            </Modal>
+            <Failure />
         </div >
     );
 }
 
-const Close = () => {
+const useInputs = () => {
+    const email = useFormInput('', value => {
+        const validator = validateEmail;
+
+        if (!value) return 'Please enter an email.';
+        else if (!validator(value)) return 'Please enter a valid email.';
+        else return '';
+    });
+
+    const password = useFormInput('', value => {
+        if (!value) return 'Please enter a password.';
+        else return '';
+    });
+
+    return Object.freeze({
+        email,
+        password,
+
+        asArray() {
+            return [this.email, this.password];
+        },
+
+        validateAll() {
+            const validate = input => input.validate() === '';
+            const isValid = (a, b) => a && b;
+            return this.asArray().map(validate).reduce(isValid, true);
+        }
+    });
+};
+
+const Failure = createModal({
+    id: "lfailure",
+    type: "failure",
+    text: "Login failed",
+});
+
+const Control = (props) => {
+    const { onClear } = props;
+
     return (
-        <a className="close modal-close has-color-black" href="#app" title="Close">Close</a>
+        <div className="control buttons is-flex mt-3">
+            <input className="close modal-close btn" type="button" value="clear" onClick={onClear} />
+            <input className="btn py-4 px-3 is-primary has-color-white" type="submit" value="Login" />
+        </div>
     );
-}
+};
 
 export default Login;

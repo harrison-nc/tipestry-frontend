7bd89ae24ff3899bb577e08bf15360b3f1547fc7
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-02-03 21:03:45 +0100

    Add getPosts function

---
+++ functions/getPosts/db/post.js
@@ -0,0 +1,124 @@
+const mongoose = require('mongoose');
+
+const ObjectId = mongoose.Types.ObjectId;
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    text: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    user: {
+        type: new Schema({
+            name: {
+                type: String,
+                default: 'annon',
+                trim: true,
+            },
+            email: {
+                type: String,
+                default: 'annon@mail.com',
+                trim: true,
+            }
+        }),
+    }
+});
+
+const schema = new Schema({
+    user: {
+        type: new Schema({
+            name: {
+                type: String,
+                trim: true,
+                default: 'annonymous'
+            },
+            email: {
+                type: String,
+                default: 'annonymous@mail.com',
+                trim: true,
+            },
+            avatarUrl: {
+                type: String,
+                default: 'https://picsum.photos/50',
+            }
+        }),
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    resourceUrl: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: String,
+    upVotes: {
+        type: Number,
+        default: 0,
+    },
+    downVotes: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    tags: [String],
+    comments: [commentSchema],
+});
+
+function createPost(post, user) {
+    const { title, resourceUrl, description, tags } = post;
+
+    const dbPost = {
+        title,
+        resourceUrl,
+        description
+    };
+
+    if (user) {
+        const { _id, name, email } = user;
+        dbPost.user = {
+            _id,
+            name,
+            email,
+        };
+    }
+
+    else dbPost.user = {};
+
+    if (tags) dbPost.tags = tags;
+
+    else dbPost.tags = [];
+
+    return new Post(dbPost).save();
+}
+
+schema.statics.create = createPost;
+
+schema.methods.addComment = async function (comment) {
+    let { text, user } = comment;
+
+    if (!user) user = {};
+
+    const _id = new ObjectId();
+    const newComment = { _id, text, user };
+
+    this.comments.push(newComment);
+
+    await this.save();
+
+    return newComment;
+}
+
+const Post = mongoose.model('posts', schema);
+
+module.exports = Post;
---
+++ functions/getPosts/getPosts.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Post = require('./db/post');
+
+const { DATABASE_URL } = process.env;
+
+const connectToDatabase = async () => {
+
+    const options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    };
+
+    return mongoose.connect(DATABASE_URL, options);
+};
+
+exports.handler = async function (event, context) {
+    const send = data => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }
+    };
+
+    const sendError = (message) => {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: message })
+        }
+    };
+
+    const getPost = async () => {
+        try {
+            await connectToDatabase();
+            console.debug('connected to database', DATABASE_URL);
+        } catch (ex) {
+            console.error(ex);
+            return sendError("Unable connect to database");
+        }
+
+        try {
+            const data = await Post.find();
+            return send(data);
+        }
+        catch (ex) {
+            console.error(ex);
+            sendError("Unable to fetch data from database");
+        }
+
+        mongoose.connection.close();
+    };
+
+    if (event.httpMethod === 'GET') {
+        return getPost();
+    }
+    else {
+        return sendError(`Request methdo ${event.httpMethod} not supported!`)
+    }
+}

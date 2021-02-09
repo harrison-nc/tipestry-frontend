33e855362ab9e4fcec246c7bc6a737d20ad9d572
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-02-09 14:00:43 +0100

    Add content type header to function response

--- functions/util/response.js
+++ functions/util/response.js
@@ -12,7 +12,8 @@ const of = data => {
             statusCode: 200,
             headers: {
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type,Accept"
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type,Accept",
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
         }

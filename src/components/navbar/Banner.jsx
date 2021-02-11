e8535b10f421a4a148609dcb45339648725747dc
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-02-11 15:17:29 +0100

    Add LineMenu to Banner component

--- src/components/navbar/Banner.jsx
+++ src/components/navbar/Banner.jsx
@@ -3,10 +3,21 @@ import { NavItem } from "./NavItem";
 
 export const Banner = ({ children }) => {
     return (
-        <div className="nav__brand">
+        <div className="nav__brand is-flex">
+            <LineMenu />
             <span className="title">
                 <NavItem link="/">{children}</NavItem>
             </span>
+        </div>
+    );
+};
+
+const LineMenu = () => {
+    return (
+        <div className="line-menu mt-6 mr-6">
+            <p className="menu__item"></p>
+            <p className="menu__item"></p>
+            <p className="menu__item"></p>
         </div>
     );
 };
